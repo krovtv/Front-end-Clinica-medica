e8a47589b3e8ee5cc84fd2ece3106f40cdde3e80
diaszin <84644190+diaszin@users.noreply.github.com>
2024-11-19 19:42:07 -0300

    Add paciente

--- sql/pre-inserts.sql
+++ sql/pre-inserts.sql
@@ -1,5 +1,21 @@
+
+-- Médicos Pré-Inseridos
 INSERT INTO medicos (nome, idade, especialidade) VALUES ('Dr. João Silva', 45, NULL), ('Dra. Maria Oliveira', 38, NULL),
 ('Dr. Carlos Santos', 50, NULL),('Dra. Ana Costa', 29, NULL),('Dr. Pedro Lima', 60, NULL),
 ('Dra. Júlia Almeida', 33, NULL), ('Dr. Ricardo Ferreira', 41, NULL),
 ('Dra. Beatriz Souza', 48, NULL),('Dr. Lucas Rocha', 37, NULL),
 ('Dra. Fernanda Ribeiro', 55, NULL);
+
+-- Pacientes Pré-Inseridos
+INSERT INTO pacientes (nome, idade, cpf) VALUES 
+('João Pereira', 30, '12345678901'),
+('Maria Silva', 25, '23456789012'),
+('Carlos Santos', 40, '34567890123'),
+('Ana Oliveira', 35, '45678901234'),
+('Pedro Costa', 50, '56789012345'),
+('Júlia Almeida', 28, '67890123456'),
+('Ricardo Lima', 45, '78901234567'),
+('Beatriz Ferreira', 32, '89012345678'),
+('Lucas Rocha', 29, '90123456789'),
+('Fernanda Ribeiro', 38, '01234567890');
+
