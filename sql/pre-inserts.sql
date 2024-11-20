013b8158d5896b036db38bdf3564ffd7d8306ddf
diaszin <84644190+diaszin@users.noreply.github.com>
2024-11-20 14:40:36 -0300

    Add tipos de exame

--- sql/pre-inserts.sql
+++ sql/pre-inserts.sql
@@ -1,11 +1,3 @@
-
--- Médicos Pré-Inseridos
-INSERT INTO medicos (nome, idade, especialidade) VALUES ('Dr. João Silva', 45, NULL), ('Dra. Maria Oliveira', 38, NULL),
-('Dr. Carlos Santos', 50, NULL),('Dra. Ana Costa', 29, NULL),('Dr. Pedro Lima', 60, NULL),
-('Dra. Júlia Almeida', 33, NULL), ('Dr. Ricardo Ferreira', 41, NULL),
-('Dra. Beatriz Souza', 48, NULL),('Dr. Lucas Rocha', 37, NULL),
-('Dra. Fernanda Ribeiro', 55, NULL);
-
 -- Pacientes Pré-Inseridos
 INSERT INTO pacientes (nome, idade, cpf) VALUES 
 ('João Pereira', 30, '12345678901'),
@@ -18,4 +10,30 @@ INSERT INTO pacientes (nome, idade, cpf) VALUES
 ('Beatriz Ferreira', 32, '89012345678'),
 ('Lucas Rocha', 29, '90123456789'),
 ('Fernanda Ribeiro', 38, '01234567890');
+
+INSERT INTO especialidades (nome, descricao) VALUES
+('Cardiologia', 'Especialidade médica que trata de doenças relacionadas ao coração e ao sistema circulatório.'),
+('Dermatologia', 'Ramo da medicina que trata da pele, cabelos, unhas e mucosas.'),
+('Neurologia', 'Área dedicada ao diagnóstico e tratamento de doenças do sistema nervoso central e periférico.'),
+('Pediatria', 'Especialidade que cuida da saúde de bebês, crianças e adolescentes.'),
+('Ortopedia', 'Ramo da medicina focado no tratamento de problemas nos ossos, músculos, articulações e ligamentos.'),
+('Gastroenterologia', 'Especialidade que trata de doenças do sistema digestivo.'),
+('Oftalmologia', 'Área da medicina que cuida dos olhos e da visão.'),
+('Psiquiatria', 'Ramo da medicina que lida com a saúde mental e transtornos psiquiátricos.'),
+('Ginecologia', 'Especialidade que trata da saúde do sistema reprodutor feminino.'),
+('Endocrinologia', 'Ramo da medicina que estuda e trata os hormônios e as glândulas do corpo.');
+
+
+INSERT INTO tipo_exame (nome, descricao) VALUES
+('Hemograma', 'Exame para avaliar a saúde geral e detectar doenças como anemia e infecções'),
+('Eletrocardiograma', 'Exame para verificar o funcionamento do coração através de sinais elétricos'),
+('Raio-X', 'Exame de imagem para análise de ossos e tecidos'),
+('Ultrassom', 'Exame de imagem para avaliar órgãos internos e gravidez'),
+('Resonância Magnética', 'Exame para obter imagens detalhadas do corpo usando campos magnéticos e ondas de rádio'),
+('Teste de Glicemia', 'Exame para medir os níveis de glicose no sangue'),
+('Endoscopia', 'Exame para visualizar o interior do trato digestivo superior'),
+('Exame de Urina', 'Análise laboratorial da urina para identificar infecções ou doenças renais'),
+('Exame de Colesterol', 'Verificação dos níveis de colesterol e lipídios no sangue'),
+('Mamografia', 'Exame de imagem para detectar anormalidades nas mamas');
+
 
