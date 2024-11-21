9cdd4288ef890b309edc7da1d0b7a0b8cf402e0d
diaszin <84644190+diaszin@users.noreply.github.com>
2024-11-20 22:59:39 -0300

    Cria scripts para criar o banco de dados

--- db.py
+++ db.py
@@ -1,14 +1,7 @@
 import sqlite3
 
 
-def __ddl_comands():
-    with open("./sql/ddl.sql", "r") as sql:
-        return sql.read()
-
-    
-
 
 with sqlite3.connect("uniruymed.db", check_same_thread=False) as conn:
-    conn.executescript(__ddl_comands())
     cursor = conn.cursor()
     
---
+++ exec_scripts_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+def ddl():
+    with open("./sql/ddl.sql", "r") as sql:
+        return sql.read()
+def pre_inserts_sql():
+    with open("./sql/pre-inserts.sql", "r", encoding='utf-8') as sql:
+        return sql.read()
+
+    
+
+
+with sqlite3.connect("uniruymed.db") as conn:
+    conn.executescript(ddl())
+    conn.executescript(pre_inserts_sql())
+
+print("Scripts executados")
